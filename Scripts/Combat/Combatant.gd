3180aaf7b06d2f94f8f0f8f0c2a2022edb85e506
mikumino <36429067+mikumino@users.noreply.github.com>
2023-03-06 00:12:26 -0800

    Combatant class
    - combatant class and some basic stuff made
    - player and enemy inherit from this

---
+++ Scripts/Combat/Combatant.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var max_hp : int = 20
+var current_hp
+@onready var health_label = $Health
+
+func _ready():
+	current_hp = max_hp
+	health_label.set_text(str(current_hp))
+
+func take_damage(dmg):
+	var remaining_hp = current_hp - dmg
+	if remaining_hp < 0:
+		remaining_hp = 0
+	current_hp = remaining_hp
+	health_label.set_text(current_hp)
+	
