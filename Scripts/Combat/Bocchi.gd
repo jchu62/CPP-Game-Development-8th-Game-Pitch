204407efcf4213457af2199a815bfbd90be40a76
mikumino <36429067+mikumino@users.noreply.github.com>
2023-03-13 15:24:34 -0700

    Random action choice
    - Bocchi now picks a random action

--- Scripts/Combat/Bocchi.gd
+++ Scripts/Combat/Bocchi.gd
@@ -1,12 +1,14 @@
 extends Combatant
 
 func choose_action(target : Combatant):
-	if current_hp != stats.HP:
-		get_parent().play_turn(self, target, "attack")	# my condolences to my fellow coders
-	else:
-		get_parent().play_turn(self, self, "defend")
-#	var action_ids = actions.keys()
-#	var chosen_action = action_ids[randi() % action_ids.size()]
+#	if current_hp != stats.HP:
+#		get_parent().play_turn(self, target, "attack")	# my condolences to my fellow coders
+#	else:
+#		get_parent().play_turn(self, self, "defend")
+	var action_ids = actions.keys()
+	var chosen_action = action_ids[randi() % action_ids.size()]
+	get_parent().play_turn(self, target, chosen_action)
+	print(chosen_action)
 # Note: placeholder for pass
 # Additional note: defend currently doesn't work for enemy
 # Although if it did it would result in an infinite loop where you can't damage the enemy
