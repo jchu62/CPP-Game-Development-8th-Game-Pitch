6edb1d21b485bcad9c90c53c24e6b47acb0d6466
jchu62 <89236068+jchu62@users.noreply.github.com>
2023-03-15 23:51:05 -0700

    Temp fix for current bugs

--- Scripts/Combat/Actions/Pass.gd
+++ Scripts/Combat/Actions/Pass.gd
@@ -2,5 +2,6 @@ extends Action
 
 func execute(actor, target):
 	print(actor.name + " is now passing!")
+	actor.animation_player.play("defend") # stupid placeholder but it works for now
+	await actor.animation_player.animation_finished
 	action_finished.emit()
-# CURRENTLY DOES NOT WORK RIGHT NOW
--- Scripts/Combat/Actions/Player Skills/Heal.gd
+++ Scripts/Combat/Actions/Player Skills/Heal.gd
@@ -3,4 +3,6 @@ extends Action
 func execute(actor, target):
 	print(actor.name + " heals!")
 	actor.heal(5)
+	actor.animation_player.play("defend") #really stupid placeholder but it works
+	await actor.animation_player.animation_finished
 	action_finished.emit()
